7ba9496699cf0dfb0996c0f567e00a1705804d1e
Toshiki Takakubo <219tosiki@gmail.com>
2024-04-25 19:29:34 +0900

    Add authentication configuration using NextAuth

---
+++ src/auth.ts
@@ -0,0 +1,29 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const config: NextAuthConfig = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  basePath: "/api/auth",
+  callbacks: {
+    authorized({ request, auth }) {
+      try {
+        const { pathname } = request.nextUrl;
+        if (pathname === "protected-page") return !!auth;
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+    jwt({ token, trigger, session }) {
+      if (trigger === "update") token.name = session.user.name;
+      return token;
+    },
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
