df3e1fae1abcbdbcd65693197774c962dc0fb1bc
Toshiki Takakubo <219tosiki@gmail.com>
2024-05-05 20:12:42 +0900

    Update CalendarPage component to use timeGridWeek as the initial view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -7,10 +6,6 @@ import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import timeGridPlugin from "@fullcalendar/timegrid";
 
 export default function CalendarPage() {
-  const [events, setEvents] = useState([
-    { title: "nice event", start: new Date(), resourceId: "a" },
-  ]);
-
   const handleDateSelect = (selectInfo: any) => {
     let title = prompt("Please enter a new event title:");
     let calendarApi = selectInfo.view.calendar;
@@ -18,45 +13,41 @@ export default function CalendarPage() {
     calendarApi.unselect(); // clear date selection
 
     if (title) {
-      calendarApi.addEvent({
+      let event = {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
         allDay: selectInfo.allDay,
-        resourceId: selectInfo.resource ? selectInfo.resource.id : null,
-      });
+      };
+      calendarApi.addEvent(event);
+      console.log(event);
     }
   };
 
   return (
-    <div>
-      <div className="w-full max-w-6xl mx-auto mt-5">
-        <FullCalendar
-          plugins={[
-            resourceTimelinePlugin,
-            dayGridPlugin,
-            interactionPlugin,
-            timeGridPlugin,
-          ]}
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "timeGridWeek",
-          }}
-          initialView="resourceTimelineWeek"
-          nowIndicator={true}
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          resources={[
-            { id: "a", title: "Auditorium A" },
-            { id: "b", title: "Auditorium B", eventColor: "green" },
-            { id: "c", title: "Auditorium C", eventColor: "orange" },
-          ]}
-          initialEvents={events}
-          select={handleDateSelect}
-        />
-      </div>
+    <div className="w-full max-w-6xl mx-auto mt-5">
+      <FullCalendar
+        plugins={[
+          resourceTimelinePlugin,
+          dayGridPlugin,
+          interactionPlugin,
+          timeGridPlugin,
+        ]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "timeGridWeek,dayGridMonth",
+        }}
+        initialView="timeGridWeek"
+        nowIndicator={true}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        initialEvents={[
+          { title: "nice event", start: new Date(), resourceId: "a" },
+        ]}
+        select={handleDateSelect}
+      />
     </div>
   );
 }
