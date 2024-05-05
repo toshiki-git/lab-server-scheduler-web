4a2a82946397766a87c09dec8f49ae23b6a1871a
Toshiki Takakubo <219tosiki@gmail.com>
2024-05-05 20:38:59 +0900

    feat: Add dropdown menu to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,10 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { signIn, signOut, auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 
@@ -10,16 +16,32 @@ export const Header = async () => {
       <div className="container flex justify-between items-center">
         <div className="text-white font-bold">Lab Scheduler</div>
         {session?.user ? (
-          <Avatar>
-            <AvatarImage
-              src={
-                session.user.image ||
-                "https://static.mercdn.net/images/member_photo_noimage_thumb.png"
-              }
-              alt={session.user.name || "User"}
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage
+                  src={
+                    session.user.image ||
+                    "https://static.mercdn.net/images/member_photo_noimage_thumb.png"
+                  }
+                  alt={session.user.name || "User"}
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <form
+                  action={async () => {
+                    "use server";
+                    await signOut();
+                  }}
+                >
+                  <Button>SignOut</Button>
+                </form>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         ) : (
           <div>
             <form
